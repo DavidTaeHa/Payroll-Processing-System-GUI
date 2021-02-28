889638f203886467c0388e9c8c8f1060c38b536f
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-02-27 21:48:41 -0500

    Added methods in controller for buttons

--- src/project3/Controller.java
+++ src/project3/Controller.java
@@ -1,9 +1,77 @@
 package project3;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 /**
  * Class that will handle all the user input and output
  *
  * @author David Ha, Andrew McAvoy
  */
 public class Controller {
+
+    /**
+     * Adds employee to the container
+     *
+     * @param mouseEvent
+     */
+    public void addEmployee(MouseEvent mouseEvent) {
+    }
+
+    /**
+     * Removes employee from the container
+     *
+     * @param mouseEvent
+     */
+    public void removeEmployee(MouseEvent mouseEvent) {
+    }
+
+    /**
+     * Sets hours for a parttime employee
+     *
+     * @param mouseEvent
+     */
+    public void setHours(MouseEvent mouseEvent) {
+
+    }
+
+    /**
+     * Imports database from a text file to the container
+     *
+     * @param actionEvent
+     */
+    public void importData(ActionEvent actionEvent) {
+    }
+
+    /**
+     * Exports database to a text file
+     *
+     * @param actionEvent
+     */
+    public void exportData(ActionEvent actionEvent) {
+    }
+
+    /**
+     * Prints employees from the container
+     *
+     * @param actionEvent
+     */
+    public void print(ActionEvent actionEvent) {
+    }
+
+    /**
+     * Prints employees from the container by date hired
+     *
+     * @param actionEvent
+     */
+    public void printByDate(ActionEvent actionEvent) {
+    }
+
+    /**
+     * Prints employees from the container by department
+     *
+     * @param actionEvent
+     */
+    public void printByDepartment(ActionEvent actionEvent) {
+    }
 }
