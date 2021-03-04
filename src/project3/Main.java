d6c28a56324fea5c2a10f7ad8e6836a006ec6cc4
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-03-04 16:21:37 -0500

    Update Main.java

--- src/project3/Main.java
+++ src/project3/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("Payroll Processing");
         primaryStage.setScene(new Scene(root, 602, 565));
         primaryStage.setResizable(false);
